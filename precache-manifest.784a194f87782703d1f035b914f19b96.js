cf29e125d3f64f19bbcb2b3fd5aa3e5ee8bc10d4
Daniel Hoffens <dhoffens@gmail.com>
2020-06-03 15:29:05 -0400

    Updates

---
+++ precache-manifest.784a194f87782703d1f035b914f19b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f261a46060ae49ea0345ac90ded36f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bbeb2c16d25a4896b776",
+    "url": "/portfolio/static/css/main.2b1c0550.chunk.css"
+  },
+  {
+    "revision": "0d16247bb461ba273639",
+    "url": "/portfolio/static/js/2.4e321521.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.4e321521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbeb2c16d25a4896b776",
+    "url": "/portfolio/static/js/main.77b376e6.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
